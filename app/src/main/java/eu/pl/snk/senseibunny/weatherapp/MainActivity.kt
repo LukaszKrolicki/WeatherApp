21ea0bfa2f0a4df2f1f871bcaebb94b9eabfbc3d
SesneiBunny <lukasmatrix12345@gmail.com>
2023-07-26 13:38:55 +0200

    first commit

---
+++ app/src/main/java/eu/pl/snk/senseibunny/weatherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package eu.pl.snk.senseibunny.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
